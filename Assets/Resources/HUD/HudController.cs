25acf46e6935c220bf8b7f8f824f868ec9440440
Juan Resinas Arias de Reyna <jresinas@emergya.com>
2021-02-03 20:29:35 +0100

    Allow to resize minimap

--- Assets/Resources/HUD/HudController.cs
+++ Assets/Resources/HUD/HudController.cs
@@ -12,6 +12,9 @@ public class HudController : MonoBehaviour
     public Text[] trapsNumber;
     public Image selectedTrap;
     private RectTransform selectedTrapFrame;
+    public MinimapCameraController minimapCamera;
+    public GameObject minimapSmall;
+    public GameObject minimapLarge;
     float y = 0;
     // Start is called before the first frame update
     void Start()
@@ -39,5 +42,19 @@ public class HudController : MonoBehaviour
 
         // Update selected trap
         selectedTrap.rectTransform.anchoredPosition = new Vector3(600f + 100f * hc.selectedTrap, selectedTrap.rectTransform.anchoredPosition.y);
+
+        // Switch minimap size
+        if (Input.GetButtonDown("ResizeMinimap")) {
+            if (minimapCamera.size == 0) {
+                minimapCamera.size = 1;
+                minimapSmall.SetActive(false);
+                minimapLarge.SetActive(true);
+            } else {
+                minimapCamera.size = 0;
+                minimapSmall.SetActive(true);
+                minimapLarge.SetActive(false);
+            }
+
+        }
     }
 }
--- Assets/Resources/HUD/Minimap/MinimapCameraController.cs
+++ Assets/Resources/HUD/Minimap/MinimapCameraController.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class MinimapCameraController : MonoBehaviour
 {
+    private float MIN_X = -15f;
+    private float MAX_X = 40f;
+    private float MIN_Z = 80f;
+    private float MAX_Z = 100f;
+
     public Camera cam;
     public Shader replacementShader;
+    public GameObject player;
+    public int size = 0;
     // Start is called before the first frame update
     void Start() {
     }
 
     // Update is called once per frame
     void Update() {
+        // In small minimap, camera follow player. In large minimap, camera is fixed
+        if (size == 0) {
+            float xPos = Mathf.Clamp(player.transform.position.x, MIN_X, MAX_X);
+            float zPos = Mathf.Clamp(player.transform.position.z, MIN_Z, MAX_Z);
+            transform.position = new Vector3(xPos, transform.position.y, zPos);
+        } else {
+            transform.position = new Vector3(10, transform.position.y, 90);
+        }
     }
 
     public void Awake() {
