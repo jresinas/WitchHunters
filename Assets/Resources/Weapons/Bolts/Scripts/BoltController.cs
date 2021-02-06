416a85ae63e068b2f29587ff8b3766e86943772c
Juan Resinas Arias de Reyna <jresinas@emergya.com>
2021-02-06 18:25:02 +0100

    Added fire and poison bolts

--- Assets/Resources/Weapons/Bolts/Scripts/BoltController.cs
+++ Assets/Resources/Weapons/Bolts/Scripts/BoltController.cs
@@ -11,11 +11,11 @@ public class BoltController : MonoBehaviour
     void Start()
     {
         //rb = GetComponent<Rigidbody>();
-
+        Destroy(gameObject, 10f);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //rb.MovePosition(rb.position + Vector3.forward * speed * Time.deltaTime);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
@@ -25,7 +25,35 @@ public class BoltController : MonoBehaviour
         if (collider.gameObject.tag == "Enemy") {
             EnemyController enemy = collider.gameObject.GetComponent<EnemyController>();
             enemy.DamageReceived(damage);
-            Destroy(gameObject);
+
+            DestroyBolt();
+        }
+        if (collider.gameObject.tag == "House" || collider.gameObject.tag == "Church") {
+            DestroyBolt();
+        }
+    }
+
+    private void DestroyBolt() {
+        if (transform.childCount > 0) {
+            DetachParticlesChild();
+        }
+        Destroy(gameObject);
+    }
+
+
+    // If arrow has particles (fire arrow, poison arrow, etc), destroy parent but keep particle alive
+    // Particles are into an empty object "ParticleWrapper" to keep the particles right size
+    private void DetachParticlesChild() {
+        Transform wrapper = transform.GetChild(0);
+
+        if (wrapper != null) {
+            ParticleSystem particle = wrapper.GetComponentInChildren<ParticleSystem>();
+
+            if (particle != null) {
+                particle.enableEmission = false;
+                wrapper.transform.parent = null;
+                Destroy(wrapper.gameObject, 5f);
+            }
         }
     }
 }
